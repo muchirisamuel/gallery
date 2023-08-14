809846670d13efa988213ff3cb5cf48b786678cc
muchirisamuel <muchirisamuel@gmail.com>
2023-08-14 05:54:47 +0300

    Update Config.js

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://muchirisamuel:Russ1a123@cluster0.vw8mngk.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://muchirisamuel:Russ1a123@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://muchirisamuel:Russ1a23@cluster0.vw8mngk.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://muchirisamuel:Russ1a123@cluster0.vw8mngk.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
